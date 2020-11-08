d5f85bb695e69e0020e8c14ea8bd7458ace79a7d
ChandruMoger <chandrumoger999@gmail.com>
2020-11-08 16:21:58 +0530

    Updates

---
+++ precache-manifest.ce6c1459c37de99ce492df7fc2d9c652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0849e5944038d910ff09",
+    "url": "/carlzeiss-task/static/css/main.2a3c2f7d.chunk.css"
+  },
+  {
+    "revision": "0849e5944038d910ff09",
+    "url": "/carlzeiss-task/static/js/main.6e157dd8.chunk.js"
+  },
+  {
+    "revision": "f1cf5e43d71198288974",
+    "url": "/carlzeiss-task/static/js/runtime~main.c7ea88d7.js"
+  },
+  {
+    "revision": "63f8e4cf5b145f5360b4",
+    "url": "/carlzeiss-task/static/js/2.bf7a807d.chunk.js"
+  },
+  {
+    "revision": "669bf559c8807da8266dd9a6f556d622",
+    "url": "/carlzeiss-task/static/media/OpenSans-Regular.669bf559.woff2"
+  },
+  {
+    "revision": "98e9ab5049f75961726e24907bb36a48",
+    "url": "/carlzeiss-task/index.html"
+  }
+];
